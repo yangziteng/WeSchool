de4e7f4caa4cc9018367c0b9391a6bb0d3246676
li1055107552 <li1055107552@163.com>
2021-10-17 22:49:06 +0800

    套个模版

--- pages/myself/myself.js
+++ pages/myself/myself.js
@@ -4,10 +4,38 @@ const app = getApp()
 
 Page({
   data: {
-
+    list:[
+      {
+        icon: "images/aboutUs.png",
+        title: "关于我们",
+        click: "about"
+      }, {
+        icon: "images/update.png",
+        title: "更新日志",
+        click: "journal"
+      }, {
+        icon: "images/login.png",
+        title: "登录/注销账号",
+        click: "login"
+      }
+    ]
   },
   onLoad() {
     
   },
+  
+  about(e){
+    console.log(e)
+    console.log("关于我们")
+  },
+  journal(e){
+    console.log(e)
+    console.log("更新日志")
+  },
+  login(e){
+    console.log(e)
+    console.log("登录/注销账号")
+  }
+
   
 })
